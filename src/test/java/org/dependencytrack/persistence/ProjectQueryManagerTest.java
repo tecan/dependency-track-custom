DependencyTrack/dependency-track

added a trigger for updating metrics for cloned project & unit tests
Signed-off-by: Joshua AL Rasbi <joshua.ar@Joshuas-MacBook-Pro.local>

src/test/java/org/dependencytrack/persistence/ProjectQueryManagerTest.java
@@ -19,15 +19,22 @@
 package org.dependencytrack.persistence;
 
 import org.dependencytrack.PersistenceCapableTest;
+import org.dependencytrack.event.ProjectMetricsUpdateEvent;
 import org.dependencytrack.model.*;
 import org.dependencytrack.tasks.scanners.AnalyzerIdentity;
 import org.junit.Assert;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+
+import alpine.event.framework.Event;
 
 import java.util.Date;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.mockito.Mockito.times;
 
 public class ProjectQueryManagerTest extends PersistenceCapableTest {
 
@@ -93,6 +100,36 @@ public class ProjectQueryManagerTest extends PersistenceCapableTest {
         assertThatExceptionOfType(IllegalArgumentException.class)
                 .isThrownBy(() -> qm.updateProject(detached, false))
                 .withMessage("Project cannot be made a collection project while it has components or services!");
+    }
+    @Test
+    public void testCloneProjectMetricUpdate() throws Exception {
+        Project project = qm.createProject("Example Project 1", "Description 1", "1.0", null, null, null, true, false);
+        Component comp = new Component();
+        comp.setId(111L);
+        comp.setName("name");
+        comp.setProject(project);
+        comp.setVersion("1.0");
+        comp.setCopyright("Copyright Acme");
+        qm.createComponent(comp, true);
+        Vulnerability vuln = new Vulnerability();
+        vuln.setVulnId("INT-123");
+        vuln.setSource(Vulnerability.Source.INTERNAL);
+        vuln.setSeverity(Severity.HIGH);
+        qm.persist(vuln);
+        qm.addVulnerability(vuln, comp, AnalyzerIdentity.INTERNAL_ANALYZER, "Vuln1", "http://vuln.com/vuln1", new Date(1708559165229L));
+
+        try (MockedStatic<Event> mockedEvent = Mockito.mockStatic(Event.class)) {
+            Project clonedProject = qm.clone(project.getUuid(), "1.1.0", false, false,
+                    true, false, false, false, false, false);
+
+            ArgumentCaptor<Event> eventCaptor = ArgumentCaptor.forClass(Event.class);
+            mockedEvent.verify(() -> Event.dispatch(eventCaptor.capture()), times(4));
+
+            Event updateCloneMetricsEvent = eventCaptor.getAllValues().get(3);
+            Assert.assertTrue(updateCloneMetricsEvent instanceof ProjectMetricsUpdateEvent);
+            ProjectMetricsUpdateEvent projectMetricsEvent = (ProjectMetricsUpdateEvent) updateCloneMetricsEvent;
+            Assert.assertEquals(clonedProject.getUuid(), projectMetricsEvent.getUuid());
+        }
     }
 
 }

tecan/dependency-track-custom

Signed-off-by: Joshua AL Rasbi <joshua.AR@ibm.com>

src/test/java/org/dependencytrack/persistence/ProjectQueryManagerTest.java
@@ -19,15 +19,22 @@
 package org.dependencytrack.persistence;
 
 import org.dependencytrack.PersistenceCapableTest;
+import org.dependencytrack.event.ProjectMetricsUpdateEvent;
 import org.dependencytrack.model.*;
 import org.dependencytrack.tasks.scanners.AnalyzerIdentity;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+
+import alpine.event.framework.Event;
 
 import java.util.Date;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.mockito.Mockito.times;
 
 class ProjectQueryManagerTest extends PersistenceCapableTest {
 
@@ -93,6 +100,36 @@ class ProjectQueryManagerTest extends PersistenceCapableTest {
         assertThatExceptionOfType(IllegalArgumentException.class)
                 .isThrownBy(() -> qm.updateProject(detached, false))
                 .withMessage("Project cannot be made a collection project while it has components or services!");
+    }
+    @Test
+    public void testCloneProjectMetricUpdate() throws Exception {
+        Project project = qm.createProject("Example Project 1", "Description 1", "1.0", null, null, null, true, false);
+        Component comp = new Component();
+        comp.setId(111L);
+        comp.setName("name");
+        comp.setProject(project);
+        comp.setVersion("1.0");
+        comp.setCopyright("Copyright Acme");
+        qm.createComponent(comp, true);
+        Vulnerability vuln = new Vulnerability();
+        vuln.setVulnId("INT-123");
+        vuln.setSource(Vulnerability.Source.INTERNAL);
+        vuln.setSeverity(Severity.HIGH);
+        qm.persist(vuln);
+        qm.addVulnerability(vuln, comp, AnalyzerIdentity.INTERNAL_ANALYZER, "Vuln1", "http://vuln.com/vuln1", new Date(1708559165229L));
+
+        try (MockedStatic<Event> mockedEvent = Mockito.mockStatic(Event.class)) {
+            Project clonedProject = qm.clone(project.getUuid(), "1.1.0", false, false,
+                    true, false, false, false, false, false);
+
+            ArgumentCaptor<Event> eventCaptor = ArgumentCaptor.forClass(Event.class);
+            mockedEvent.verify(() -> Event.dispatch(eventCaptor.capture()), times(4));
+
+            Event updateCloneMetricsEvent = eventCaptor.getAllValues().get(3);
+            Assertions.assertTrue(updateCloneMetricsEvent instanceof ProjectMetricsUpdateEvent);
+            ProjectMetricsUpdateEvent projectMetricsEvent = (ProjectMetricsUpdateEvent) updateCloneMetricsEvent;
+            Assertions.assertEquals(clonedProject.getUuid(), projectMetricsEvent.getUuid());
+        }
     }
 
 }